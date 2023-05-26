1857896afe97b4349b4130ee402e65e9ee49db8d
Evlantiya <serfer.po.kiskam1337@gmail.com>
2023-05-27 03:02:41 +0500

    refactored organizations controller

--- src/main/java/com/test/CRM/controllers/OrganizationController.java
+++ src/main/java/com/test/CRM/controllers/OrganizationController.java
@@ -9,6 +9,7 @@ import com.test.CRM.controllers.Exceptions.WrongIdFormatException;
 
 import com.test.CRM.models.Organization;
 import com.test.CRM.repositories.OrganizationRepository;
+import com.test.CRM.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -16,59 +17,40 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class OrganizationController {
 
-    //TODO Инкапсулировать
-    @Autowired
-    OrganizationRepository organizationRepository;
+    private final OrganizationService organizationService;
 
-    //TODO переместить реализацию методов в сервис
+    @Autowired
+    public OrganizationController(OrganizationService organizationService){
+        this.organizationService=organizationService;
+    }
+
     @PostMapping("/resources/organizations")
     public Organization create(@RequestBody Organization organization){
-        organizationRepository.save(organization);
-        return organization;
+        return organizationService.create(organization);
     }
 
     @GetMapping("/resources/organizations")
     public List<Organization> retrieveOrganizations(){
-
-        List<Organization> organizations = new ArrayList<Organization>();
-
-        for(Organization o : organizationRepository.findAll()){
-            organizations.add(o);
-        }
-
-        return organizations;
+        return organizationService.retrieveOrganizations();
     }
 
     @GetMapping("/resources/organizations/{id}")
     public Organization retrieveOrganization(@PathVariable String id){
 
-        Optional<Organization> organization;
-        try{
-            organization = organizationRepository.findById(Integer.parseInt(id));
-        }catch(NumberFormatException e){
-            throw new WrongIdFormatException();
-        }
-
-        if(organization == null || organization.isEmpty()){
-            throw new ElementNotFoundException(id);
-        }
-
-        return organization.get();
+        return organizationService.retrieveOrganization(id);
     }
 
     @PutMapping("/resources/organizations/{id}")
     public Organization updateOrganization(@PathVariable String id, @RequestBody Organization organization){
 
-        organizationRepository.save(organization);
-
-        return organization;
+        return organizationService.updateOrganization(id,organization);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/resources/organizations/{id}")
     public void deleteOrganization(@PathVariable String id){
         try{
-            organizationRepository.deleteById( Integer.parseInt(id));
+            organizationService.deleteOrganization(id);
         }catch(NumberFormatException e){
             throw new WrongIdFormatException();
         }
--- src/main/java/com/test/CRM/services/OrganizationService.java
+++ src/main/java/com/test/CRM/services/OrganizationService.java
@@ -1,4 +1,84 @@
 package com.test.CRM.services;
 
+import com.test.CRM.controllers.Exceptions.ElementNotFoundException;
+import com.test.CRM.controllers.Exceptions.WrongIdFormatException;
+import com.test.CRM.models.Organization;
+import com.test.CRM.models.Role;
+import com.test.CRM.models.User;
+import com.test.CRM.repositories.OrganizationRepository;
+import com.test.CRM.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
 public class OrganizationService {
+    private final OrganizationRepository organizationRepository;
+
+    @Autowired
+    public OrganizationService( OrganizationRepository  organizationRepository)
+    {
+        this.organizationRepository = organizationRepository;
+    }
+
+    public Organization create(@RequestBody Organization organization){
+        organizationRepository.save(organization);
+        return organization;
+    }
+
+    public List<Organization> retrieveOrganizations(){
+
+        List<Organization> organizations = new ArrayList<Organization>();
+
+        for(Organization o : organizationRepository.findAll()){
+            organizations.add(o);
+        }
+
+        return organizations;
+    }
+
+    public Organization retrieveOrganization(String id){
+
+        Optional<Organization> organization;
+        try{
+            organization = organizationRepository.findById(Integer.parseInt(id));
+        }catch(NumberFormatException e){
+            throw new WrongIdFormatException();
+        }
+
+        if(organization == null || organization.isEmpty()){
+            throw new ElementNotFoundException(id);
+        }
+
+        return organization.get();
+    }
+
+    public Organization updateOrganization(String id, Organization organization){
+
+        organizationRepository.save(organization);
+
+        return organization;
+    }
+
+    public void deleteOrganization(String id){
+        try{
+            organizationRepository.deleteById( Integer.parseInt(id));
+        }catch(NumberFormatException e){
+            throw new WrongIdFormatException();
+        }
+    }
+
+    public Organization save(Organization organization){
+        organizationRepository.save(organization);
+        return organization;
+    }
+
 }
